18ecca6d7bb5c5abd3e29e2749d16406ff46fb1b
karunakrishna <karuna.krishna@exterro.com>
2018-12-26 16:11:35 +0530

    Commit by Karuna

---
+++ Yahoo/src/main/java/pack1/HomeController.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/CallMe")
+public class HomeController {
+	@Path("/show")
+	@POST
+	public void showMessage(){
+		System.out.println("Hello Monu!!!");
+	}
+
+}
